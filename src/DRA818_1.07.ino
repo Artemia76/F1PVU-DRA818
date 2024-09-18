d543f355b88d7125530691f62425ce9d58ddd23f
Artemia76 <neophile76@gmail.com>
2024-09-18 19:45:40 +0200

    Amelioration du programme

--- src/DRA818_1.07.ino
+++ src/DRA818_1.07.ino
@@ -1,51 +1,56 @@
-#include <easyRun.h>
+// Calibration des seuils pour le S mètre
+// Limitation Fréquences à la bande 2 mètres ( bande passante ampli limitée )
+// Limitation du S mètre
+// Augmentaion du temps de présentation et modif auteurs
+// 1.09 - Reprise Complète par F4IKZ(Gianni) pour intégrer plusieurs corrections :
+//  * Librairies long support arduino (SchedTask)
+//  * Migration du projet sur platformio pour automatiser les dépendances
+//  * Reprise de la gestion du codeur rotatif bidirectionnel
+//  * Synchronisation de l'envoi des messages de mise à jour du DRA818 pour éviter les sauts de fréquences
+//  * Mémorisation dans l'eeprom des pas de fréquence, mode répéteur, mode reverse
 
+// Inclusion des dépendances
+#include <EEPROM.h>
+#include <Wire.h>
+#include "LiquidCrystal_I2C.h"
+#include <BoutonPin.h>
+#include <SchedTask.h>
+#include <SoftwareSerial.h>
+
+// Affectation des E/S
+#define codeurRotatifA_3  3   // CLK du codeur Rotatif
+#define codeurRotatifB_2  2   // DT du codeur Rotatif
+#define PUCH              7   // Bouton poussoir du codeur rotatif
+#define SHIFT             11  // Entrée SHIFT 600
+#define PTT               9   // Bouton PTT
+#define REV               10  // EntréeREV
+#define hzPTT             6   // Sortie Tone 1750 Hz
+#define sPTT              4   // sortie PTT
+#define sRSSi             5   // sortie RSSi
+#define DRA818_TX         8   // TX UART
+#define DRA818_RX         12  // RX UART
+
+// Définition des constantes
+// Limite de fréquence basse en Hz
+#define LIMB              144000000
+// Limite de fréquence haute en Hz
+#define LIMH              146000000
+
+#define DEBUG             1
 // Version programme
 String vers = "V1.09";
-
-//Calibration des seuils pour le S mètre
-//Limitation Fréquences à la bande 2 mètres ( bande passante ampli limitée )
-//Limitation du S mètre
-//Augmentaion du temps de présentation et modif auteurs
-
-
-#include <EEPROM.h>
-
-//Afficheur
-#include "Wire.h"
-#include "LiquidCrystal_I2C.h"
-#include <Bouton.h>
-#include "easyRun.h"
 
 // définit le type d'écran lcd 16 x 2
 LiquidCrystal_I2C LCD(0x27,16,2);
 
-// Encodeur rotatif
-volatile boolean modif;
-volatile boolean up;
+SoftwareSerial DRA818 (DRA818_RX, DRA818_TX);
 
-// Affectation des PIN
-#define codeurRotatifA_3  3   // CLK Rotary
-#define codeurRotatifB_2  2   // DT Rotary
-#define PUCH              7   // sw Rotary
-#define SHIFT             11  // Entrée SHIFT 600
-#define PTT               9   // Bouton PTT
-#define REV               10  // EntréeREV
-
-#define hzPTT 6    // Sortie Tone 1750 Hz
-#define sPTT 4     // sortie PTT
-#define sRSSi 5     // sortie RSSi
-
-Bouton fdPUCH(PUCH,1,1);
-Bouton fSHIFT(SHIFT,1,1);
-Bouton fPTT(PTT,1,1);
-Bouton fREV(REV,1,1);
-
-// Définition des constantes
-// Limite de fréquence basse
-#define LIMB  144000000
-// Limite de fréquence haute
-#define LIMH  146000000
+// Afectation des boutons poussoirs 
+// Pour la gestion anti rebond 
+BoutonPin fdPUCH(PUCH, true, true);
+BoutonPin fSHIFT(SHIFT, true, true);
+BoutonPin fPTT(PTT, true, true);
+BoutonPin fREV(REV, true, true);
 
 // Définition des variables
 boolean A_set = true;
@@ -53,105 +58,109 @@ boolean B_set = true;
 boolean readyB = false;
 boolean mSHIFT = false;
 boolean mREV = false;
+boolean bModeConfig = false;
+boolean bAModeConfig = bModeConfig;
+boolean bDispChange = false;
 
-int Npas;
-int val;
-int RSSi;
-int adresse_frx;
-int adresse_pas;
-int adresse_rpt;
-int adresse_rev;
-long Decaltx;
-long Decalrx;
-long frx;
-long Pas;
+int Npas = 1;
+int RSSi = 0;
+int squ = 1;
+int smetre = 0;
 
+long Decaltx = 0;
+long Decalrx = 0;
+long frx = LIMB;
+long Pas = 12500;
+
+// Définition des adresses mémoires pour le stockage de l'état
+// du transceiver
+int adresse_frx = 0;
+int adresse_pas = adresse_frx + sizeof(frx); // Pas
+int adresse_rpt = adresse_pas + sizeof(Pas); // Mode Répéteur
+int adresse_rev = adresse_rpt + sizeof(mSHIFT); // Mode Reverse
 
 // paramètres réglés pour DRA818V
 
 // Bande passante en kHz ( 0 = 12.5 or 1 = 25 )
 String bw = "0";
+
 // fréquence de ctcss (0000 - 0038); 0000 = "no CTCSS"
 String tx_ctcss = "0000";
+
 // fréquence de ctcss (0000 - 0038); 0000 = "no CTCSS"
 String rx_ctcss = "0000";
 
-//Réglage du squelch
-// niveau du silencieux (0-8); 0 = ouvert"
-String squ = "0";
+// RSSI Callback
+void RSSI ();
+SchedTask RSSI_Timer (0, 400, RSSI);
 
-//tâches périodiques
-asyncTask lanceur;
-
-// Routine asynchrone  RSSI
+// Routine asynchrone RSSI
 void RSSI ()
 {
   String RRSSI = "";
-  Serial.println ("RSSI?"); //demande RSSI au module
-  Serial.flush();
-  LCD.setCursor(13, 0);
-  LCD.print("   ");
-  while (Serial.available() > 0)
+  DRA818.println ("RSSI?"); //demande RSSI au module
+  DRA818.flush();
+  delay(10);
+  while (DRA818.available() < 9)
   {
-    char v = Serial.read();
-    RRSSI += v;
+    delay(10);
   }
+  while (DRA818.available() > 0)
+  {
+    char c = DRA818.read();
+    RRSSI += c;
+  }
+  #ifdef DEBUG
+  Serial.println(RRSSI);
+#endif
   RRSSI = RRSSI.substring(5,8);
 
-  int vrssi =(RRSSI.toInt());
-  int svrssi = map(vrssi, 0, 255, 0, 255);      //Mise a l'echelle b_source,_h_source,b_desti,h_desti Olivier
-  LCD.setCursor(13, 0);                                 //Affichage RSSI Olivier
-  LCD.print(svrssi);                                    //Affichage RSSI Olivier
-  RRSSI = "";
-                                                                //Debut reglage RSSI Olivier
-  if (svrssi == 0)analogWrite(sRSSi, 0);                            
-  if ((svrssi > 0) && (svrssi < 36))analogWrite(sRSSi, 0);           //S0     
-  if ((svrssi > 36) && (svrssi < 41))analogWrite(sRSSi, 7);          //S1
-  if ((svrssi > 41) && (svrssi < 48))analogWrite(sRSSi, 15);         //S2
-  if ((svrssi > 48) && (svrssi < 54))analogWrite(sRSSi, 23);         //S3
-  if ((svrssi > 54) && (svrssi < 57))analogWrite(sRSSi, 35);         //S4
-  if ((svrssi > 57) && (svrssi < 61))analogWrite(sRSSi, 50);         //S5
-  if ((svrssi > 61) && (svrssi < 67))analogWrite(sRSSi, 60);         //S6
-  if ((svrssi > 67) && (svrssi < 72))analogWrite(sRSSi, 70);         //S7
-  if ((svrssi > 72) && (svrssi < 81))analogWrite(sRSSi, 80);         //S8
-  if ((svrssi > 81) && (svrssi < 92))analogWrite(sRSSi, 95);         //S9
-  if ((svrssi > 92) && (svrssi < 98))analogWrite(sRSSi, 110);        //S9+10
-  if ((svrssi > 98) && (svrssi < 109))analogWrite(sRSSi, 125);       //S9+20
-  if ((svrssi > 110) && (svrssi < 224))analogWrite(sRSSi, 135);      //Au dela de S9+20
-  //if ((svrssi > 254) && (svrssi < 255))analogWrite(sRSSi, 140);
-  //Fin reglage RSSI Olivier
-  
-  //Frequence de mise à jour en msec RSSI Olivier
-  lanceur.set(RSSI, 400);
+  RSSi =(RRSSI.toInt());
+  smetre = map(RSSi,36,224,0,135);
+  if (smetre < 0) smetre = 0;
+  analogWrite(sRSSi, smetre);
+
+  bDispChange = true;
 }
 
-// Routine Envoi message au SA818
+// Routine Envoi message au DRA818
 
 void Envoi()
 {
-  String Ret = "";
+  String buffer = "";
   //Envoi Data au DRA818
-  Serial.print ("AT+DMOSETGROUP="); // commencer un message
-  Serial.print (bw);
-  Serial.print (",");
-  Serial.print ((frx - Decalrx)/ 1000000.0, 4);
-  Serial.print (",");
-  Serial.print ((frx - Decaltx)/ 1000000.0, 4);
-  Serial.print (",");
-  Serial.print (tx_ctcss);
-  Serial.print (",");
-  Serial.print (squ);
-  Serial.print (",");
-  Serial.println (rx_ctcss);
-  Serial.flush();
+  buffer = "AT+DMOSETGROUP=";
+  buffer += String(bw);
+  buffer += String(",");
+  buffer += String((frx + Decaltx)/ 1000000.0, 4);
+  buffer += String(",");
+  buffer += String((frx + Decalrx)/ 1000000.0, 4);
+  buffer += String(",");
+  buffer += String(tx_ctcss);
+  buffer += String(",");
+  buffer += String(squ);
+  buffer += String(",");
+  buffer += String(rx_ctcss);
+  DRA818.println(buffer);
+  DRA818.flush();
   
-  // On attend le code retour du SA818
-  while (Serial.available() > 0)
+#ifdef DEBUG
+  Serial.println(buffer);
+#endif
+  buffer = "";
+  while (DRA818.available() < 9)
   {
-    char v = Serial.read();
-    Ret += v;
+    delay(10);
   }
-
+  // On attend le code retour du SA818
+  while (DRA818.available() > 0)
+  {
+    char c = DRA818.read();
+    buffer += c;
+  }
+#ifdef DEBUG
+  Serial.println(buffer);
+#endif
 }
 
 // Gestion de l'interruption codeur sans le sens horaire
@@ -192,37 +201,31 @@ void Config_RPT ()
 {
   if (mSHIFT)                                  //Memorisation de D11
   {
-    LCD.setCursor(0, 1);                                //Curseur LCD 0 ligne 1
-    LCD.print("RPT");                                   //ecriture RPT sur le LCD
-    Decalrx = 600000;                                   // force le decalage rx a 600
+    Config_REV ();
   }
   else
   {
-    mREV = 0;
-    LCD.setCursor(0, 1);                                //Curseur LCD 0 ligne 1
-    LCD.print("           ");                           //effacement RPT sur le LCD
-    Decaltx = 0;                                        // force le decalage tx a 0
-    Decalrx = 0;                                        // force le decalage rx a 0
+    mREV = false;
+    Decaltx = 0;
+    Decalrx = 0; 
   }
+  bDispChange = true;
 }
 
 /// @brief Gestion du mode reverse en mode répéteur
 void Config_REV ()
 {
   if (mREV)                                     //Memorisation de D11
-    {
-      LCD.setCursor(5, 1);                                //Curseur LCD 0 ligne 1
-      LCD.print("REV");                                   //ecriture RPT sur le LCD
-      Decaltx = 600000;                                   // force le decalage tx a 600 kHz
-      Decalrx = 0;                                       // force le decalage rx a 0
-    }
-    else
-    {
-      LCD.setCursor(5, 1);                                //Curseur LCD 0 ligne 1
-      LCD.print("   ");                                   //effacement RPT sur le LCD
-      Decaltx = 0;                                        // force le decalage tx a 0
-      Decalrx = -600000;                                  // force le decalage rx a -600 kHz
-    }
+  {
+    Decaltx = 0;                                   // force le decalage tx a 600 kHz
+    Decalrx = 600000;                                       // force le decalage rx a 0
+  }
+  else
+  {
+    Decaltx = 600000;                                        // force le decalage tx a 0
+    Decalrx = 0;                                  // force le decalage rx a -600 kHz
+  }
+  bDispChange = true;
 }
 
 /// @brief Configuration initiale de l'arduino
@@ -230,32 +233,26 @@ void Config_REV ()
 void setup()
 {
   Serial.begin (9600);
-  lanceur.set(RSSI, 0);
+  pinMode(DRA818_RX, INPUT);
+  pinMode(DRA818_TX, OUTPUT);
+  DRA818.begin (9600);
+  
+  // Affectation de la sortie pour activer le ton 1750 Hz
   pinMode(hzPTT, OUTPUT);
+  
+  // Affectation de la sortie pour piloter le PTT
   pinMode(sPTT, OUTPUT);
+  digitalWrite (sPTT, LOW);
+
+  // Affectation de la sortie analogique pour l'indication du signal
   pinMode(sRSSi, OUTPUT);
-  pinMode(codeurRotatifA_3, INPUT_PULLUP); //Encodeur rotatif
-  pinMode(codeurRotatifB_2, INPUT_PULLUP); //Econdeur rotatif
 
-  digitalWrite (sPTT, LOW);       //Sortie PTT
-
+  // Affectation des interruptions matérielles pour le codeur VFO
   attachInterrupt (digitalPinToInterrupt(codeurRotatifA_3), codeurA, CHANGE);
   attachInterrupt (digitalPinToInterrupt(codeurRotatifB_2), codeurB, CHANGE);
-  //lanceur.set(Lecture, 400);      //tâches périodiques definition du temps lecture port serie
-  adresse_frx = 0;
-  adresse_pas = adresse_frx + sizeof(frx);
-  adresse_rpt = adresse_pas + sizeof(Pas);
-  adresse_rev = adresse_rpt + sizeof(mSHIFT);
-  Decalrx = 0;
-  Decalrx = 0;
-  Pas = 12500; // 12.5 kHz
-  squ = 1;
-  Npas = 1;
-  modif = false;
-  mSHIFT = false;
-  mREV = false;
-  val = 0;
-  RSSi = 0;
+
+  fdPUCH.setLongPressDelay(3000);
+  fdPUCH.setLongPressInterval(3000);
 
   // lecture du pas dans l'eprom
    EEPROM.get(adresse_pas,Pas);
@@ -279,13 +276,7 @@ void setup()
 
   // lecture de frx dans l'eprom
   EEPROM.get(adresse_rev,mREV);
-  if (mREV && !mSHIFT)
-  {
-    mREV = false;
-    EEPROM.put(adresse_rev,mREV); 
-  }
 
-  //-------------- Affichage lors de l'initialisation  -------------
   LCD.init(); // initialisation de l'afficheur
   LCD.backlight();
   LCD.setCursor(1, 0);
@@ -295,22 +286,84 @@ void setup()
   LCD.print("F1PVU / DL " + vers);
   delay(1000);
   LCD.clear();
-  LCD.setCursor(0, 0);
-  LCD.print(frx / 1000000.0, 4);  // Fréquence affichée au démarrage.
-  LCD.setCursor(12, 1);
-  LCD.print(Pas/1000.0,1);
   Config_RPT ();
-  Config_REV ();
   Envoi();
+  bDispChange = true;
 }
 
-/// @brief Programme principal de l'arduino
-
-void loop()
+void Update_Display()
 {
-  // Lancement de la tâche asynchrone 
-  easyRun();
-  
+  if (bAModeConfig != bModeConfig)
+  {
+    bAModeConfig = bModeConfig;
+    LCD.clear();
+    bDispChange = true;
+  }
+  if (bDispChange)
+  {
+    bDispChange = false;
+    if (bModeConfig)
+    {
+      LCD.setCursor(0,0);
+      LCD.print("CONFIGURATION");
+    }
+    else
+    {
+      // Affichage de la fréquence de réception
+      LCD.setCursor(0, 0);
+      LCD.print((frx + Decalrx) / 1000000.0, 4);
+
+      if (mSHIFT)
+      {
+        LCD.setCursor(8, 0);
+        LCD.print("R");
+        LCD.setCursor(0, 1);
+        LCD.print((frx + Decaltx) / 1000000.0, 4);
+        LCD.setCursor(8, 1);
+        LCD.print("T");
+      }
+      else
+      {
+        LCD.setCursor(8, 0);
+        LCD.print(" ");
+        LCD.setCursor(0, 1);
+        LCD.print("         ");
+      }
+      // Affichage du pas de fréquence en kHz
+      LCD.setCursor(12, 1);
+      LCD.print(Pas/1000.0,1);
+      if (mSHIFT)
+      {
+        LCD.setCursor(10, 0);                                //Curseur LCD 0 ligne 1
+        LCD.print("RP");                                   //ecriture RPT sur le LCD    Decaltx = 600000;                                   // force le decalage tx a 600 kHz
+      }
+      else
+      {
+        LCD.setCursor(10, 0);                                //Curseur LCD 0 ligne 1
+        LCD.print("  ");                                   //effacement RPT sur le LCD
+      }
+      if (mREV)
+      {
+        LCD.setCursor(10, 1);                              //Curseur LCD 0 ligne 1
+        LCD.print("R");                                 //ecriture RPT sur le LCD
+      }
+      else
+      {
+        LCD.setCursor(10, 1);                                //Curseur LCD 0 ligne 1
+        LCD.print(" ");                                   //effacement RPT sur le LCD
+      }
+      LCD.setCursor(13, 0);                                 //Affichage RSSI Olivier
+      char buffer[4];
+      snprintf(buffer , sizeof buffer, "%03d", RSSi);
+      LCD.print(buffer);                                    //Affichage RSSI Olivier
+    }
+  }
+
+}
+
+/// @brief Gestion du poste en mode traffic
+void mode_traffic()
+{
   //  Appel 1750 Hz : Entrée D9 en mode poussoir qui active la sortie PTT ( D5 ) et génère une tonalité à 1750 Hz sur la sortie D6
   fPTT.refresh();                                       
   if (fPTT.isOnPress())                                //Front descendant de D9
@@ -376,18 +429,48 @@ void loop()
       default:
         Pas = 12500;
     }
-    LCD.setCursor(12, 1);
-    LCD.print(Pas/1000.0,1);
     EEPROM.put(adresse_pas,Pas);          // ecriture de frx dans l'eprom
+    bDispChange = true;
   }
  
+  if(fdPUCH.isLongPressed())
+  {
+    bModeConfig = true;
+  }
+
   // Si la fréquence a changé
   if ( readyB )
   {
-      LCD.setCursor(0, 0);
-      LCD.print((frx / 1000000.0), 4);
+      readyB=false;
       EEPROM.put(adresse_frx,frx);          // ecriture de frx dans l'eprom
       Envoi();
-      readyB=false;
+      bDispChange=true;
   }
+}
+
+/// @brief Gestion du poste en mode configuration
+void mode_config()
+{
+  fdPUCH.refresh();
+  if(fdPUCH.isLongPressed())
+  {
+    bModeConfig = false;
+  }
+}
+
+/// @brief Programme principal de l'arduino
+
+void loop()
+{
+  if (bModeConfig)
+  {
+    mode_config();
+  }
+  else
+  {
+    mode_traffic();
+  }
+  // Execution des tâches asynchrones
+  SchedBase::dispatcher();
+  Update_Display ();
 }
